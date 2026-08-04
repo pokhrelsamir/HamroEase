f207b04c7d77e14c3f134e55911ee5fde9d19b2b
Samir Pokhrel <poksamir6@gmail.com>
2026-08-04 20:07:30 +0545

    Pagination Search Feature API Construction

--- config/settings.py
+++ config/settings.py
@@ -210,6 +210,12 @@ REST_FRAMEWORK = {
         "rest_framework.filters.SearchFilter",
         "rest_framework.filters.OrderingFilter",
     ),
+
+    # Pagination
+    "DEFAULT_PAGINATION_CLASS": (
+        "rest_framework.pagination.PageNumberPagination"
+    ),
+    "PAGE_SIZE": 10,
 }
 
 from datetime import timedelta
