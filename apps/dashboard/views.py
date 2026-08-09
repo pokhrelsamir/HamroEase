8e182e5549b0fbb70fbadf4c27a7c791537a3523
Samir Pokhrel <poksamir6@gmail.com>
2026-08-09 20:42:20 +0545

    Dashboard views.py file Update

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def dashboard(request):
+
+    user = request.user
+
+    if user.role == "HOTEL_MANAGER":
+        template = "dashboard/manager_dashboard.html"
+
+    elif user.role == "ADMIN":
+        template = "dashboard/admin_dashboard.html"
+
+    else:
+        template = "dashboard/guest_dashboard.html"
+
+    return render(request, template)
